bfb32d3afd55a1c7eb3f92e3d7f90f85146781c6
Jussi Kupari <jkupari@KI-CND1457CS2.user.ki.se>
2023-01-24 15:12:47 +0100

    Add some comment to _targets.R

--- _targets.R
+++ _targets.R
@@ -15,11 +15,16 @@ tar_option_set(
 )
 
 list(
+  # target raw data, metadata, fragments
   tar_target(raw_data, here("data", "atac_v1_pbmc_10k_filtered_peak_bc_matrix.h5"), format = "file"),
   tar_target(meta, here("data", "atac_v1_pbmc_10k_singlecell.csv"), format = "file"),
   tar_target(frags, here("data", "atac_v1_pbmc_10k_fragments.tsv.gz"), format = "file"),
+  
+  # Load data
   tar_target(counts, get_data(raw_data)),
   tar_target(metadata, get_metadata(meta)),
+  
+  # Create Chromatin Assay, Seurat object and preprocess object
   tar_target(chrom_assay, create_chrom_assay(counts, frags)),
   tar_target(
     pbmc, 
@@ -28,6 +33,8 @@ list(
       compute_qc() %>% 
       modify_metadata()
   ),
+  
+  # Plot some qc
   tar_target(qc_plots, plot_qc(pbmc)),
   tar_target(qc_violins, plot_qc_violins(pbmc))
 )
